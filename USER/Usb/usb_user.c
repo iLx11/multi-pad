5cc6818996d69cb8124513a3304dae9e78edcdfb
iLx1 <c.olor-ful@qq.com>
2023-09-19 21:15:13 +0800

    MENU_HARDWARE_TEST_DONE

--- USER/Usb/usb_user.c
+++ USER/Usb/usb_user.c
@@ -35,7 +35,7 @@ extern uint8_t oled_96_array[OLED_96_NUM][SIZE_96];
 extern char json_str[JSON_SIZE];
 
 uint8_t folder_index = 0;
-uint8_t file_array_index = 25;
+uint8_t file_array_index = 0;
 uint16_t file_position = 0;
 uint8_t photo_file_flag = 1;
 
@@ -96,7 +96,7 @@ void receive_data_from_upper(USBD_CUSTOM_HID_HandleTypeDef *hid_handle, uint8_t 
             /* ----------------- test ----------------------------------*/
             file_array_index = 0;
             file_position = 0;
-            folder_index > 9 ? folder_index = 0 : folder_index ++;
+            folder_index += 1;
         } else {
             strncat(json_str, (char *) hid_handle->Report_buf, 64);
             file_position += 64;
@@ -104,6 +104,12 @@ void receive_data_from_upper(USBD_CUSTOM_HID_HandleTypeDef *hid_handle, uint8_t 
     }
 }
 
+/********************************************************************************
+* 设置键值
+********************************************************************************/
+static void set_key_value(const char *string_key_setting) {
+
+}
 
 /********************************************************************************
 * 将16进制的字符串图片数组转为数值并写入图片数组
